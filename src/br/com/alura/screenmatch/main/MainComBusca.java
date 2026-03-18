7955148d3bcb78ca6b7a8f6797dcea7487aa8314
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-03-18 12:47:47 -0300

    Implementando comunicação Web-Service

--- src/br/com/alura/screenmatch/main/MainComBusca.java
+++ src/br/com/alura/screenmatch/main/MainComBusca.java
@@ -1,9 +1,22 @@
 package br.com.alura.screenmatch.main;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class MainComBusca {
-    static void main(String[] args) {
+    static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=the+amazing+spider-man&apikey=29b58849"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
 
     }
-
 
 }
